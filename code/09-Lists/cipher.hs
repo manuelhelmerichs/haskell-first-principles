f1218ddb89f492e132e840736fb4185375120ddc
manuel helmerichs <manuelloehrmann@gmail.com>
2021-12-10 11:09:38 +0100

    cipher

---
+++ code/09-Lists/cipher.hs
@@ -0,0 +1,36 @@
+module Cipher where
+
+import Data.Char
+
+caesar :: Int -> String -> String
+caesar _ [] = []
+caesar n (x:xs) = shiftRight n x : caesar n xs
+
+unCaesar :: Int -> String -> String
+unCaesar _ [] = []
+unCaesar n (x:xs) = shiftLeft n x : unCaesar n xs
+
+shiftRight :: Int -> Char -> Char
+shiftRight a b
+  | isUpper b = shiftRightUpper a b
+  | isLower b = shiftRightLower a b
+  | otherwise = b
+
+shiftLeft :: Int -> Char -> Char
+shiftLeft a b
+  | isUpper b = shiftLeftUpper a b
+  | isLower b = shiftLeftLower a b
+  | otherwise = b
+
+shiftRightLower :: Int -> Char -> Char
+shiftRightLower a b = chr $ ((a + (ord b - ord 'a')) `mod` 26) + ord 'a'
+
+shiftRightUpper :: Int -> Char -> Char
+shiftRightUpper a b = chr $ ((a + (ord b - ord 'A')) `mod` 26) + ord 'A'
+
+shiftLeftLower :: Int -> Char -> Char
+shiftLeftLower a b = chr $ (((ord b - ord 'a') - a) `mod` 26) + ord 'a'
+
+shiftLeftUpper :: Int -> Char -> Char
+shiftLeftUpper a b = chr $ (((ord b - ord 'A') - a) `mod` 26) + ord 'A'
+
--- code/09-Lists/filter.hs
+++ code/09-Lists/filter.hs
@@ -1,3 +1,4 @@
+import Data.Char -- for isLower
 
 -- how filter is implemented in Prelude
 filter' :: (a -> Bool) -> [a] -> [a]
@@ -18,4 +19,18 @@ myFilter :: String -> [String]
 myFilter s = filter articles $ words s
   where articles :: String -> Bool
         articles x = x /= "the" && x /= "a" && x /= "an"
+
+allCaps :: String -> String
+allCaps s = filter isUpper s
+
+capFirst :: String -> String
+capFirst [] = []
+capFirst (x:xs) = toUpper x : xs
+
+capAll :: String -> String
+capAll [] = []
+capAll (x:xs) = toUpper x : capAll xs
+
+capHead :: String -> Char
+capHead = toUpper . head
 
